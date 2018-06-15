b04de0081512dffde9e8b185029d604a3d9a4210
Jeff Niu <jeffniu22@gmail.com>
2018-06-15 14:27:10 -0400

    create and destroy

--- include/wlib/mem/Detail.h
+++ include/wlib/mem/Detail.h
@@ -4,7 +4,12 @@
 namespace wlp {
     namespace det {
         template<bool Cond, typename T = void>  struct enable_type_if {};
-        template<typename T>                    struct enable_type_if<true, T> { typedef T type; };
+        template<           typename T>         struct enable_type_if<true, T> { typedef T type; };
+
+        template<typename T> struct is_alloc_primitive :
+            public or_<is_arithmetic<T>, is_pointer<T>>::type {};
+        template<typename T> struct is_alloc_complex :
+            public and_<is_object<T>, not_<is_alloc_primitive<T>>>::type {};
     }
 }
 
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,24 +1,68 @@
 #include <stdio.h>
-
-#include <wlib/mem/Construct.h>
+#include <wlib/memory>
 
 typedef unsigned char byte;
 
 using namespace wlp;
+
+static char s_label[] = "None";
 
 struct items {
     int count;
     double fraction;
     const char *label;
 
+    items() : count(1), fraction(0), label(s_label) {}
     items(int t_count, double t_fraction, const char *t_label) :
         count(t_count), fraction(t_fraction), label(t_label) {}
+    ~items() {
+        printf("~items()\n");
+    }
+
+    void print() 
+    { printf("%i, %.2f, %s\n", count, fraction, label); }
 };
 
 int main(int argc, char *argv[]) {
-    static byte pool[512];
+    static byte memory[1024];
+    static byte pool[2 * sizeof(items)];
     static char label[] = "Items";
-    items *ptr = construct<items>(pool, 5, 10.15, label);
+    mem::init(memory, 1024);
 
-    printf("%i, %.2f, %s\n", ptr->count, ptr->fraction, ptr->label);
+    int *iptr;
+    items *ptr;
+    unsigned long *lptr;
+
+    // construct
+    ptr = construct<items>(pool, 5, 10.15, label);
+    ptr->print();
+    deconstruct(ptr);
+
+    lptr = construct<unsigned long>(pool, 167);
+    printf("%i\n", *lptr);
+    deconstruct(lptr);
+
+    // create
+    iptr = create<int>(25);
+    printf("%i\n", *iptr);
+    destroy(iptr);
+
+    ptr = create<items>(55, 66.67, label);
+    ptr->print();
+    destroy(ptr);
+
+    iptr = create<int[]>(4);
+    iptr[0] = 1;
+    iptr[1] = 2;
+    iptr[2] = 4;
+    iptr[3] = 8;
+    printf("%i, %i, %i, %i\n", iptr[0], iptr[1], iptr[2], iptr[3]);
+    destroy<int[]>(iptr);
+
+    ptr = create<items[]>(4);
+    ptr[0].print();
+    ptr[1].print();
+    ptr[2].print();
+    ptr[3].print();
+    destroy<items[]>(ptr);
 }
