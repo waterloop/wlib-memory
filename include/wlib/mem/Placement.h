863a5582871ce75255fca49c7418663554334956
Mogball <jeffniu22@gmail.com>
2018-09-29 18:10:04 -0400

    Remove definition of placement new

--- include/wlib/mem/Placement.h
+++ include/wlib/mem/Placement.h
@@ -1,6 +1,6 @@
 #ifndef __WLIB_MEMORY_PLACEMENT_H__
 #define __WLIB_MEMORY_PLACEMENT_H__
 
-void *operator new(decltype(sizeof(0)), void *ptr) noexcept { return ptr; }
+extern void *operator new(decltype(sizeof(0)), void *ptr) noexcept;
 
 #endif
